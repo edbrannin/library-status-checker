1c8f3a1e8c8007d5406e0a4a88387eb2b5002a86
Ed Brannin <ed.brannin@skillsoft.com>
2019-06-11 17:20:06 -0400

    Fix tests

--- test_check_status.py
+++ test_check_status.py
@@ -6,19 +6,18 @@ today = arrow.now().floor('day')
 yesterday = today.shift(days=-1)
 tomorrow = today.shift(days=+1)
 
-
 def test_days_left_tomorrow():
-    loan = Loan(dict(dueDate=tomorrow.timestamp * 1000))
+    loan = Loan(dict(dueDate=tomorrow.timestamp * 1000, renewalCount=0))
     print today
     assert loan.days_left == 1
     assert not loan.is_overdue
 
 def test_days_left_today():
-    loan = Loan(dict(dueDate=today.timestamp * 1000))
+    loan = Loan(dict(dueDate=today.timestamp * 1000, renewalCount=0))
     assert loan.days_left == 0
     assert not loan.is_overdue
 
 def test_days_left_yesterday():
-    loan = Loan(dict(dueDate=yesterday.timestamp * 1000))
+    loan = Loan(dict(dueDate=yesterday.timestamp * 1000, renewalCount=0))
     assert loan.days_left == -1
     assert loan.is_overdue
